11a4c582ee9c704aecd089386c0791bb143c8546
MrDemiurgos <davidvazquez@id.uff.br>
2023-12-14 14:47:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from mywindow import *
 def main():
     app = QApplication(sys.argv)
     gui = MyWindow()
+
+    gui.setGeometry(100, 100, 800, 600)
+    gui.setStyleSheet("background-color: #1E1E1E; color: #FFFFFF; font-size: 16px;")
+
     gui.show()
     sys.exit(app.exec_())
 
