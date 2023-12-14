dfa58bd17a91ac3f7dc3a6185a985c37d530d223
MrDemiurgos <davidvazquez@id.uff.br>
2023-12-14 15:02:54 -0300

    update

--- mywindow.py
+++ mywindow.py
@@ -14,21 +14,14 @@ class MyWindow(QMainWindow):
         self.textBox = TextBox()
         self.canvas = MyCanvas(self.textBox)
         self.setCentralWidget(self.canvas)
-        # create a model object and pass to canvas
         self.model = MyModel()
         self.canvas.setModel(self.model)
 
-        # create a Toolbar
         tb = self.addToolBar("File")
-
-       # add insertText
         boxText = QAction(QIcon(r"icons\radius.png"),"particulas",self)
         tb.addAction(boxText)
-
-        # add insertText
         saveJson = QAction(QIcon(r"icons\json.png"),"saveJson",self)
         tb.addAction(saveJson)
-
         tb.actionTriggered[QAction].connect(self.tbpressed)
 
 
